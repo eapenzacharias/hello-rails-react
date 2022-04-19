9bf23aa0fdbdf4ecde76840dc1fe32dabe60fa94
eapenzacharias <eapenzac@gmail.com>
2022-04-20 00:35:06 +0200

    Created message controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api, default: {format: :json} do
+    resources :greetings, only: [:index] do
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
