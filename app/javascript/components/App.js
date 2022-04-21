0bb1c9f388c38445bb953e8f6a5281621cdb86e8
eapenzacharias <eapenzac@gmail.com>
2022-04-21 12:39:58 +0200

    Added store and messages in the greeting page

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
 import Greetings from './Greetings'
 const App = () => {
   return (
     <>
       <h1>App</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/greetings" element={<Greetings />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/greetings" element={<Greetings />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
--- app/javascript/components/Greetings.js
+++ app/javascript/components/Greetings.js
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMessage } from '../redux/greeting/greeting';
 const Greeting = () => {
+  const message = useSelector(state => state.message);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMessage());
+  }, []);
   return (
-    <h2>Greetings</h2>
+    <>
+      <h2>Greeting</h2>
+      <h2>{message}</h2>
+    </>
   )
 }
 
