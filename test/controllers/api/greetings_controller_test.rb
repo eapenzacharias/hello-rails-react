976b83c2cd9069efbbe4cfbfb8c83a6b98f397df
eapenzacharias <eapenzac@gmail.com>
2022-04-21 13:05:50 +0200

    Deleted unused test files

--- test/controllers/api/greetings_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class Api::GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get api_greetings_index_url
-    assert_response :success
-  end
-end
