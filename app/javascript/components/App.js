320a8ae31cf8c13883c4fa31f5de66a41f318e76
eapenzacharias <eapenzac@gmail.com>
2022-04-21 12:45:36 +0200

    Added Nav links to app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import Greetings from './Greetings'
@@ -9,6 +9,8 @@ const App = () => {
       <h1>App</h1>
       <Provider store={store}>
         <BrowserRouter>
+          <NavLink to="/">Home</NavLink> <br />
+          <NavLink to="/greetings">Greetings</NavLink>
           <Routes>
             <Route path="/" element={<h1>Home</h1>} />
             <Route path="/greetings" element={<Greetings />} />
